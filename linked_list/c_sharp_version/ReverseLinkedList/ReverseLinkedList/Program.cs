6380c6e25cfa4c1123cbcc2e00a7ebb4e5e4c540
shanemcg88 <shanelgmcguire@gmail.com>
2021-07-17 15:58:07 -0600

    oops

---
+++ linked_list/c_sharp_version/ReverseLinkedList/ReverseLinkedList/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace ReverseLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+
+            linkedList.Insert(1);
+            linkedList.Insert(2);
+            linkedList.Insert(3);
+            linkedList.Insert(4);
+            linkedList.Insert(5);
+
+            linkedList.Traverse();
+
+            linkedList.Reverse();
+            linkedList.Traverse();
+        }
+
+        public class Node
+        {
+            public int? data;
+            public Node nextNode = null;
+
+            public Node(Nullable<int> dataInput)
+            {
+                data = dataInput;
+            }
+        }
+
+        public class LinkedList
+        {
+            public Node head;
+            
+            public void Insert(int data)
+            {
+                Node newNode = new Node(data);
+                Node currentNode = head;
+
+                if (head == null)
+                    head = newNode;
+                else
+                {
+                    while (currentNode.nextNode != null)
+                    {
+                        currentNode = currentNode.nextNode;
+                        
+                    }
+                    currentNode.nextNode = newNode;
+                }
+
+            }
+
+            public void Reverse()
+            {
+                Node currentNode = head;
+                Node nextNode = null;
+                Node prevNode = null;
+
+                while (currentNode.nextNode != null)
+                {
+                    nextNode = currentNode.nextNode;
+                    currentNode.nextNode = prevNode;
+                    prevNode = currentNode;
+                    currentNode = nextNode;    
+                }
+
+                head = prevNode;
+            }
+
+            public void Traverse()
+            {
+                Node currentNode = head;
+                if (head != null)
+                {
+                    while (currentNode != null)
+                    {
+                        Console.WriteLine(currentNode.data);
+
+                        currentNode = currentNode.nextNode;
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ linked_list/c_sharp_version/linkedlist/linkedlist/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace linkedlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var linkedList = new LinkedList();
+            linkedList.InsertStart(3);
+            linkedList.InsertStart(2);
+            linkedList.InsertStart(1);
+            linkedList.InsertEnd(50);
+            linkedList.Traverse();
+            linkedList.SizeOfList();
+
+            Console.WriteLine("---------------");
+
+            linkedList.Remove(2);
+            linkedList.Traverse();
+            linkedList.SizeOfList();
+
+            linkedList.Remove(4);
+
+        }
+    }
+
+    public class Node
+    {
+        public int? data;
+        public Node nextNode = null;
+        public Node(Nullable<int> dataInput)
+        {
+            data = dataInput;
+        }
+    }
+
+    public class LinkedList
+    {
+        public int numOfNodes = 0;
+        public Node head = null;
+
+        public void InsertStart(int data)
+        {
+            numOfNodes += 1;
+            var newNode = new Node(data);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                newNode.nextNode = head;
+                head = newNode;
+            }
+        }
+
+        public void InsertEnd(int data)
+        {
+            numOfNodes += 1;
+            var newNode = new Node(data);
+            var actualNode = head;
+
+            while (actualNode.nextNode != null)
+            {
+                actualNode = actualNode.nextNode;
+            }
+            actualNode.nextNode = newNode;
+        }
+
+        public void Remove(int data)
+        {
+            if (head == null)
+                return;
+
+            Node previousNode = new Node(null);
+            Node actualNode = head;
+
+            do
+            {
+                previousNode = actualNode;
+                actualNode = actualNode.nextNode;
+
+                if (actualNode == null)
+                {
+                    Console.WriteLine("Couldn't find item: " + data);
+                    return;
+                }
+
+                if (actualNode.data == data || previousNode == null)
+                {
+                    previousNode.nextNode = actualNode.nextNode;
+                    numOfNodes -= 1;
+                }
+            }
+            while (actualNode.data != data && actualNode != null);
+
+        }
+
+        public int SizeOfList()
+        {
+            return numOfNodes;
+        }
+
+        public void Traverse()
+        {
+            Node actualNode = head;
+
+            while (actualNode != null)
+            {
+                Console.WriteLine(actualNode.data);
+                actualNode = actualNode.nextNode;
+            }
+        }
+    }
+}
